8ef39a378b1f1c1dfa05404f2b9f4f83b611819e
Adenilson <adekonzelmann@gmail.com>
2025-03-04 07:13:18 -0300

    feat: application

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole cluster-admin \
   --user $(gcloud config get-value account)
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.0-beta.0/deploy/static/provider/cloud/deploy.yaml
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.yaml
+
+
 #Intalação do prom-grafana para avaliar consumo de recursos do argo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
